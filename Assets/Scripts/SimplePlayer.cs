996b64da359fcf9fd5329c9b2eafc9b55d94cd07
Paweł Maczuga <pmaczuga97@gmail.com>
2020-04-03 19:49:18 +0200

    Added zombie, simple palyer, project folders

---
+++ Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayer : MonoBehaviour
+{
+    public float speed = 7.5f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float yPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        yPos = transform.position.y; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float curSpeedX = speed * Input.GetAxis("Vertical");
+        float curSpeedY = speed * Input.GetAxis("Horizontal");
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+        Vector3 temp = transform.position;
+        temp.y = yPos;
+        transform.position = temp;
+    }
+}
---
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float distanceToAttack = 1.8f;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+    private int attackHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        attackHash = Animator.StringToHash("Base Layer.Attack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+        if (agent.remainingDistance < distanceToAttack)
+        {
+            Vector3 lookPos = player.position - transform.position;
+            lookPos.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+            agent.isStopped = true;
+            animator.SetBool("Attack", true);
+        } 
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).fullPathHash != attackHash)
+        {
+            agent.isStopped = false;
+        }
+    }
+}
